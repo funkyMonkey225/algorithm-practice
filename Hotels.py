1f57142f1eacb70543f46138bdeee7ed18a159bf
Mac User <sabbey37@gmail.com>
2017-09-08 14:35:49 -0400

    Updates hotels.py

--- Hotels.py
+++ Hotels.py
@@ -1,13 +1,18 @@
 
-def hotels(input1, input2):
+def hotels():
+    input1 = raw_input("")
+    input2 = raw_input("")
+
+    input1 = input1.split(" ")
+    input2 = input2.split(" ")
     highestTotal = 0
     i = 0
-    for i in range(input1[0]):
+    for i in range(int(input1[0]):
         tempTotal = 0
         counter = 0
-        while total <= input1[1] and counter < input1[0]:
-            if (tempTotal + input2[counter]) <= max:
-                tempTotal = tempTotal + input2[counter]
+        while total <= int(input1[1]) and counter < int(input1[0]):
+            if (tempTotal + int(input2[counter]) <= max:
+                tempTotal = tempTotal + int(input2[counter])
             counter =  counter + 1
         if tempTotal > highestTotal:
             highestTotal = tempTotal
