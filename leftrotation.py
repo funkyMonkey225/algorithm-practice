2b75c92672ce4f9410660fede15c4c706ddc0b48
Mac User <sabbey37@gmail.com>
2017-09-08 10:05:51 -0400

    Updates leftrotation.py

---
+++ leftrotation.py
@@ -0,0 +1,48 @@
+# A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. For example, if left rotations are performed on array , then the array would become .
+
+# Given an array of  integers and a number, , perform  left rotations on the array. Then print the updated array as a single line of space-separated integers.
+
+# Input Format
+
+# The first line contains two space-separated integers denoting the respective values of  (the number of integers) and  (the number of left rotations you must perform). 
+# The second line contains  space-separated integers describing the respective elements of the array's initial state.
+
+# Constraints
+
+# Output Format
+
+# Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
+
+# Sample Input
+
+# 5 4
+# 1 2 3 4 5
+# Sample Output
+
+# 5 1 2 3 4
+# Explanation
+
+# When we perform  left rotations, the array undergoes the following sequence of changes:
+
+# Thus, we print the array's final state as a single line of space-separated values, which is 5 1 2 3 4.
+
+for(int i = 0; i < lengthOfArray; i++) {
+    int newLocation = (i + (lengthOfArray - shiftAmount)) % lengthOfArray;
+    a[newLocation] = in.nextInt();
+}
+
+def array_left_rotation(a, n, k):
+    for i in range(k):
+        temp = a[0]
+        for j in range(n):
+            if j < n-1:
+                a[j] = a[j+1]
+            else:
+                a[j] = temp
+    return a
+  
+
+n, k = map(int, raw_input().strip().split(' '))
+a = map(int, raw_input().strip().split(' '))
+answer = array_left_rotation(a, n, k);
+print ' '.join(map(str,answer))
